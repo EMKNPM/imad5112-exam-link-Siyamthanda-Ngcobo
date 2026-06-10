66395744ba85fe9cb3b7e9d029a14a6d68d2cc53
Siyamthanda-Ngcobo <ST10530817@myemeris.edu.za>
2026-06-10 10:44:43 +0200

    Back-end
    Splash screen back end is completed

--- app/src/main/java/com/example/imad/AddGear.kt
+++ app/src/main/java/com/example/imad/AddGear.kt
@@ -1,28 +1,16 @@
 package com.example.imad
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class MainActivity : AppCompatActivity() {
+class AddGear : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        //Handler is used to display the next action
-        android.os.Handler(Looper.getMainLooper()).postDelayed({
-
-        //Create an intent to move from one activity to the register activity
-            val intent= Intent(this, AddGear::class.java)
-            startActivity(intent)
-
-        },3000) //3 seconds = 3000
+        setContentView(R.layout.activity_add_gear)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
