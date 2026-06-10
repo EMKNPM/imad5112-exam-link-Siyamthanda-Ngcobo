5ae2b947bd55abcd0fdd3024be0c311a50ebefbe
Siyamthanda-Ngcobo <ST10530817@myemeris.edu.za>
2026-06-10 11:17:18 +0200

    Declaration
    Declarations for the main screen completed

--- app/src/main/java/com/example/imad/AddGear.kt
+++ app/src/main/java/com/example/imad/AddGear.kt
@@ -1,16 +1,32 @@
 package com.example.imad
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AddGear : AppCompatActivity() {
+
+    //Declaration
+
+    private lateinit var btnAddGear : Button
+    private lateinit var tvTitle : TextView
+    private lateinit var tvTotalItemsPacked : TextView
+    private lateinit var edtComments : EditText
+    private lateinit var edtQuantity : EditText
+    private lateinit var edtCategory : EditText
+    private lateinit var edtItemName : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_gear)
+
+        //
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
